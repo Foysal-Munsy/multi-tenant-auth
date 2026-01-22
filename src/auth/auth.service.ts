1a47883bc7f2a08664d0e27b9753984fd05e636c
Foysal-Munsy <bdfoysal555@gmail.com>
2026-01-22 14:41:25 +0600

    in the constructor -inject mongoose module to perform DB quries. -added jwt service to sign and verify JSON web tokens.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Organization } from 'src/schemas/organization.schema';
+import { UserOrgMap } from 'src/schemas/user-org-map.schema';
+import { User } from 'src/schemas/user.schema';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    // 1.User
+    @InjectModel(User.name) private userModel: Model<User>,
+    // 2.Organization
+    @InjectModel(Organization.name) private orgModel: Model<Organization>,
+    // 3.UserOrgMap
+    @InjectModel(UserOrgMap.name) private mapModel: Model<UserOrgMap>,
+    // 4.Jwt
+    private jwtService: JwtService,
+  ) {}
+}
