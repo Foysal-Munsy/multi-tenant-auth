8998150fd465eaa13d6c3da76c4d535b97be1be1
Foysal-Munsy <bdfoysal555@gmail.com>
2026-01-22 14:24:58 +0600

    [added] import mongooseModule to register schemas.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/schemas/user.schema';
+import {
+  Organization,
+  OrganizationSchema,
+} from 'src/schemas/organization.schema';
+import { UserOrgMap, UserOrgMapSchema } from 'src/schemas/user-org-map.schema';
 
 @Module({
+  imports: [
+    //1. schema register
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Organization.name, schema: OrganizationSchema },
+      { name: UserOrgMap.name, schema: UserOrgMapSchema },
+    ]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
