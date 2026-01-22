308a4d8ad61c9ad3e081ef9aa4418268f75c3216
Foysal-Munsy <bdfoysal555@gmail.com>
2026-01-22 13:58:56 +0600

    [added] auth resource.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/multi_tenant')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/multi_tenant'),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
