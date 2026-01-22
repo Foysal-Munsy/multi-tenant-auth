ac35ae214ffa58d8aef984e16de9043cf209c2b0
Foysal-Munsy <bdfoysal555@gmail.com>
2026-01-22 14:04:17 +0600

    [added] register-dto to prevents invalid input, defines API contract, keeps controller clean.

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class RegisterDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  org_id?: string;
+}
